417f761f84cddb07bd24b92deb35ebeb340846e0
Ubuntu <ubuntu@ip-172-31-91-33.ec2.internal>
2019-08-21 15:20:13 +0000

    Connected the database successfully to the application and inserted a recipe from my recipes collection on to the recipes page

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'cook_book'
-app.config["MONGO_URI"] = 'MONGO_URI'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
     
 @app.route('/recipes')
 def recipes():
-    return render_template('recipes.html')
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
