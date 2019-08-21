5028f047902f375d05f3479571b6cc36be0422c2
Ubuntu <ubuntu@ip-172-31-91-33.ec2.internal>
2019-08-21 16:58:14 +0000

    Added addrecipe page

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ def index():
 @app.route('/recipes')
 def recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
